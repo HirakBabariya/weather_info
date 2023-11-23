3e0400e037d4569341a3ac0042ea2639876b7814
HirakBabariya <82439424+HirakBabariya@users.noreply.github.com>
2023-11-23 17:36:01 -0500

    Add files via upload

---
+++ app.py
@@ -0,0 +1,29 @@
+from flask import Flask, render_template, request
+import requests
+
+app=Flask(__name__)
+
+@app.route('/')
+def homepage():
+    return render_template("index.html")
+
+@app.route("/weather",  methods=['GET', 'POST'])
+def get_weatherdata():
+    url = "https://weatherapi-com.p.rapidapi.com/forecast.json"
+    params = {
+        "q" : request.args.get("city")
+    }
+    headers = {
+        "X-RapidAPI-Key": "4ded612ff1mshfa97ab85255debep179561jsn0fae02ef241b",
+        "X-RapidAPI-Host": "weatherapi-com.p.rapidapi.com"
+    }
+
+    response = requests.get(url, headers=headers, params=params)
+    data = response.json()
+    city=data['location']['name']
+    temp=data['current']['temp_c']
+    return f"City: {city}, Tempreture: {temp} \n All Other Data are ::: {data}"
+
+
+if __name__=='__main__':
+    app.run(host="0.0.0.0", port=5000)
